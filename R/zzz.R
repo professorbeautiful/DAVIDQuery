96399f4d85ddc43ad6939c5fe77bf28ff0da28e3
r.day <r.day@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-09-02 03:43:27 +0000

    Fixing NOTES and one ERROR from bioconductor check.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/DAVIDQuery@108039 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,13 @@
-.First.lib <- function(libname, pkgname) {
-	### .First.lib is NOT RECOMMENDED for packages with namespace, but it works.
-	data(DAVIDURLBase)
-	data(DAVIDToolChoices)
-	data(DAVIDTypeChoices)
-	data(DAVIDAnnotChoices)
-	data(DAVIDAffyChipChoices)
-	data(idExampleList)
-}
 .onAttach = function(libname, pkgname) {
-	desc <- packageDescription("DAVIDQuery")
+  data(DAVIDURLBase)
+  data(DAVIDToolChoices)
+  data(DAVIDTypeChoices)
+  data(DAVIDAnnotChoices)
+  data(DAVIDAffyChipChoices)
+  data(idExampleList)
+  desc <- packageDescription("DAVIDQuery")
 	DQdate <-  desc$Date
 	DQVersion =  desc$Version
-	cat("This is DAVIDQuery Version ", DQVersion , " ", DQdate, "\n")
+	packageStartupMessage("This is DAVIDQuery Version ", DQVersion , " ", DQdate, "\n")
 	return(invisible(NULL))
 }
