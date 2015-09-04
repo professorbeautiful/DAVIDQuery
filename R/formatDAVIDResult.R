65e0dd4ea1ec0a26c0764438a55391765b598fb6
Roger Day <notsofeo@gmail.com>
2015-09-04 12:27:55 -0400

    Corrected mishandling of the attributes in formatDAVIDResult.

--- R/formatDAVIDResult.R
+++ R/formatDAVIDResult.R
@@ -1,18 +1,23 @@
 formatDAVIDResult <- function(result, verbose=FALSE) {
 	### we always use read.delim(...header=TRUE) but formatting expects the first row tobe the column names
 	### in order to make formatting work we add the top row
-	result<-rbind(colnames(result),result);
 
-	tool <- attr(result,"tool") 
-	if(verbose) 
-		cat("formatDAVIDResult: tool=", tool, 
-			ifelse(tool=="geneReportFull", 
-			" (invisible return)", ""), 
-		"\n")
-	### formatting depends on which is done
-	if(tool=="geneReportFull") {
-		returnval <- try(formatGeneReportFull(result))
-	} else if(tool=="geneReport") {
+  tool <- attr(result,"tool") 
+  annot <- attr(result,"annot") 
+  ids <- attr(result,"ids") 
+  type <- attr(result,"type")
+  
+  result<-rbind(colnames(result),result);
+  
+  if(verbose) 
+    cat("formatDAVIDResult: tool=", tool, 
+        ifelse(tool=="geneReportFull", 
+               " (invisible return)", ""), 
+        "\n")
+  ### formatting depends on which is done
+  if(tool=="geneReportFull") {
+    returnval <- try(formatGeneReportFull(result))
+  } else if(tool=="geneReport") {
 		returnval <- try(formatGeneReport(result))
 	} else if(tool=="list") {
 		returnval <- try(formatList(result))
@@ -25,6 +30,6 @@ formatDAVIDResult <- function(result, verbose=FALSE) {
 	if(class(returnval) == "try-error")
 		returnval <- result
 	for(attname in c("annot", "ids", "tool", "type"))
-		attr(returnval, attname) <- attr(result, attname)
+		attr(returnval, attname) <- get(attname)
 	returnval
 }
